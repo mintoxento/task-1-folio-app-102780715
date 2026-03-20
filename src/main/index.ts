30afbc45357bcceac03b2f00694be6b1ff6ea9ec
JuneH <168982270+mintoxento@users.noreply.github.com>
2026-03-21 01:42:52 +0800

    bug fixes

--- src/main/index.ts
+++ src/main/index.ts
@@ -95,7 +95,7 @@ function createWindow(): BrowserWindow {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.folio')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
